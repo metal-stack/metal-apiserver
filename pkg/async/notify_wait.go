425e5e7485a699c1b3b25be9d66aea81f3b9ceab
Gerrit <tirreg91@googlemail.com>
2025-03-14 11:09:15 +0100

    Generics.

---
+++ pkg/async/notify_wait.go
@@ -0,0 +1,149 @@
+package async
+
+import (
+	"context"
+	"encoding/json"
+	"log/slog"
+	"time"
+
+	"github.com/redis/go-redis/v9"
+)
+
+type (
+	Notifier[E any] interface {
+		// Notify allows adding an element to the stream which will then be delivered to the waiters.
+		// messages fan out to all waiters.
+		Notify(ctx context.Context, elem E) error
+	}
+	Waiter[E any] interface {
+		// Wait receives elements from the stream starting from the point in time when this function is called.
+		// when the context is done, the channel gets closed.
+		Wait(ctx context.Context) <-chan E
+	}
+
+	notifier[E any] struct {
+		client     *redis.Client
+		streamName string
+		log        *slog.Logger
+	}
+
+	waiter[E any] struct {
+		client     *redis.Client
+		streamName string
+		log        *slog.Logger
+	}
+)
+
+const (
+	maxLength    = 1000
+	elemAccessor = "e"
+)
+
+func New[E any](log *slog.Logger, client *redis.Client, streamName string) (Notifier[E], Waiter[E]) {
+	return &notifier[E]{
+			log:        log.WithGroup("notify").With("stream-name", streamName),
+			streamName: streamName,
+			client:     client,
+		}, &waiter[E]{
+			log:        log.WithGroup("waiter").With("stream-name", streamName),
+			streamName: streamName,
+			client:     client,
+		}
+}
+
+func (n *notifier[E]) Notify(ctx context.Context, elem E) error {
+	encoded, err := json.Marshal(elem)
+	if err != nil {
+		return err
+	}
+
+	value := map[string]string{
+		elemAccessor: string(encoded),
+	}
+
+	result, err := n.client.XAdd(ctx, &redis.XAddArgs{
+		Stream: n.streamName,
+		MaxLen: maxLength,
+		Values: value,
+	}).Result()
+	if err != nil {
+		return err
+	}
+
+	n.log.Info("notify", "id", result)
+
+	return nil
+}
+
+func (w *waiter[E]) Wait(ctx context.Context) <-chan E {
+	var (
+		resChan = make(chan E)
+		lastID  = "$"
+	)
+
+	go func() {
+		for {
+			w.log.Info("waiting for stream entries", "from-id", lastID)
+
+			results, err := w.client.XRead(ctx, &redis.XReadArgs{
+				Streams: []string{w.streamName},
+				Block:   0,
+				Count:   1,
+				ID:      lastID,
+			}).Result()
+			if err != nil {
+				select {
+				case <-ctx.Done():
+					close(resChan)
+					return
+				default:
+					w.log.Error("error reading from stream", "error", err)
+					time.Sleep(500 * time.Millisecond)
+					continue
+				}
+			}
+
+			w.log.Debug("received result", "result", results)
+
+			if len(results) != 1 {
+				w.log.Error("received more than a single result but used count 1, what is redis doing?")
+				continue
+			}
+
+			result := results[0]
+
+			if len(result.Messages) != 1 {
+				w.log.Error("received more than a single message but used count 1, what is redis doing?")
+				continue
+			}
+
+			m := result.Messages[0]
+
+			lastID = m.ID
+			w.log.Info("received message", "id", m.ID)
+
+			encoded, ok := m.Values[elemAccessor]
+			if !ok {
+				w.log.Error("element is not contained in msg values")
+				continue
+			}
+
+			encodedString, ok := encoded.(string)
+			if !ok {
+				w.log.Error("encoded element is malformed")
+				continue
+			}
+
+			var elem E
+			err = json.Unmarshal([]byte(encodedString), &elem)
+			if err != nil {
+				w.log.Error("unable to decode stream element", "error", err)
+				continue
+			}
+
+			resChan <- elem
+		}
+	}()
+
+	return resChan
+}
--- pkg/async/notify_wait_test.go
+++ pkg/async/notify_wait_test.go
@@ -1,26 +1,32 @@
-package fanout
+package async_test
 
 import (
 	"context"
+	"fmt"
 	"log/slog"
 	"os"
 	"testing"
 	"time"
 
+	"github.com/metal-stack/metal-apiserver/pkg/async"
 	"github.com/metal-stack/metal-apiserver/pkg/test"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
 func Test_notifier_NotifyAndWait(t *testing.T) {
-	ctx := context.Background()
 	log := slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
 
-	container, client, err := test.StartValkey(t, ctx)
+	container, client, err := test.StartValkey(t, context.Background())
 	require.NoError(t, err)
 	defer func() {
-		_ = container.Terminate(ctx)
+		_ = container.Terminate(context.Background())
 	}()
+
+	oneThousandMachines := make([]string, 0, 1000)
+	for i := range 1000 {
+		oneThousandMachines = append(oneThousandMachines, fmt.Sprintf("happy-waiting-machine-%d", i))
+	}
 
 	tests := []struct {
 		name       string
@@ -32,38 +38,39 @@ func Test_notifier_NotifyAndWait(t *testing.T) {
 			machineIDs: []string{"m-100"},
 			wantErr:    false,
 		},
-
 		{
 			name:       "add more machines",
 			machineIDs: []string{"m-1", "m-2", "m-3", "m-4", "m-5", "m-6", "m-7", "m-8", "m-9", "fw-1"},
 			wantErr:    false,
 		},
+		{
+			name:       "add a thousand machines",
+			machineIDs: oneThousandMachines,
+			wantErr:    false,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			ctx, cancel := context.WithTimeout(ctx, 2*time.Second)
+			ctx, cancel := context.WithTimeout(context.Background(), 2*time.Second)
 			defer cancel()
 
-			n := &notifier{
-				client: client,
-				log:    log,
-			}
-			w := &waiter{
-				client: client,
-				log:    log,
-			}
+			n, w := async.New[string](log, client, tt.name)
 
 			var machineIDs []string
 
 			go func() {
 				for mid := range w.Wait(ctx) {
-					t.Logf("got machineID:%s", mid)
+					t.Logf("got machineID: %s", mid)
+
 					machineIDs = append(machineIDs, mid)
+
 					if len(machineIDs) == len(tt.machineIDs) {
 						cancel()
 					}
 				}
 			}()
+
+			time.Sleep(100 * time.Millisecond) // this one's important!
 
 			for _, m := range tt.machineIDs {
 				if err := n.Notify(ctx, m); (err != nil) != tt.wantErr {
@@ -72,8 +79,8 @@ func Test_notifier_NotifyAndWait(t *testing.T) {
 			}
 
 			<-ctx.Done()
-			assert.ElementsMatch(t, tt.machineIDs, machineIDs)
 
+			assert.ElementsMatch(t, tt.machineIDs, machineIDs)
 		})
 	}
 }
