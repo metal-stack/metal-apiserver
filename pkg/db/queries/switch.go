23c43b6a4b322ceb436c4f1e8a3f6f42f4e6de0e
Ilja Rotar <kontakt@ilja-rotar.de>
2025-09-03 15:04:21 +0200

    switch query

--- pkg/db/queries/switch.go
+++ pkg/db/queries/switch.go
@@ -10,7 +10,38 @@ func SwitchFilter(query *apiv2.SwitchQuery) func(q r.Term) r.Term {
 		return nil
 	}
 	return func(q r.Term) r.Term {
-		// TODO: implement
+		if query.Id != nil {
+			q = q.Filter(func(row r.Term) r.Term {
+				return row.Field("id").Eq(*query.Id)
+			})
+		}
+
+		if query.Partition != nil {
+			q = q.Filter(func(row r.Term) r.Term {
+				return row.Field("partition").Eq(*query.Partition)
+			})
+		}
+
+		if query.Rack != nil {
+			q = q.Filter(func(row r.Term) r.Term {
+				return row.Field("rack").Eq(*query.Rack)
+			})
+		}
+
+		if query.Os != nil {
+			if query.Os.Vendor != nil {
+				q = q.Filter(func(row r.Term) r.Term {
+					return row.Field("os").Field("vendor").Eq(query.Os.Vendor.String())
+				})
+			}
+
+			if query.Os.Version != nil {
+				q = q.Filter(func(row r.Term) r.Term {
+					return row.Field("os").Field("version").Eq(*query.Os.Version)
+				})
+			}
+		}
+
 		return q
 	}
 }
