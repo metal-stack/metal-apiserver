477f9240991df3e959c999bec7af5ea412ab45ab
Stefan Majer <stefan.majer@f-i-ts.de>
2025-10-15 18:11:32 +0200

    fix validation interceptor test

--- cmd/server/test/validation-interceptor_test.go
+++ cmd/server/test/validation-interceptor_test.go
@@ -1,63 +1,52 @@
 package test
 
-// import (
-// 	"context"
-// 	"net/http"
-// 	"net/http/httptest"
-// 	"testing"
+import (
+	"log/slog"
+	"net/http"
+	"net/http/httptest"
+	"testing"
 
-// 	"connectrpc.com/connect"
-// 	"connectrpc.com/validate"
-// 	apiv2 "github.com/metal-stack/api/go/metalstack/api/v2"
-// 	"github.com/metal-stack/api/go/metalstack/api/v2/apiv2connect"
-// 	"github.com/stretchr/testify/require"
-// )
+	"connectrpc.com/connect"
+	"connectrpc.com/validate"
+	apiv2 "github.com/metal-stack/api/go/metalstack/api/v2"
+	"github.com/metal-stack/api/go/metalstack/api/v2/apiv2connect"
+	"github.com/metal-stack/metal-apiserver/pkg/service/token"
+	"github.com/stretchr/testify/require"
+)
 
-// func TestWithValidator(t *testing.T) {
-// 	t.Parallel()
-// 	interceptor := validate.NewInterceptor()
+func TestWithValidator(t *testing.T) {
+	t.Parallel()
+	interceptor := validate.NewInterceptor()
 
-// 	mux := http.NewServeMux()
-// 	mux.Handle(apiv2connect.TokenServiceCreateProcedure, connect.NewUnaryHandler(
-// 		apiv2connect.TokenServiceCreateProcedure,
-// 		// createToken,
-// 		func(ctx context.Context, _ *apiv2.TokenServiceCreateRequest) (*apiv2.TokenServiceCreateResponse, error) {
+	mux := http.NewServeMux()
+	mux.Handle(apiv2connect.NewTokenServiceHandler(token.New(token.Config{
+		Log: slog.Default(),
+	}), connect.WithInterceptors(interceptor)))
+	srv := startHTTPServer(t, mux)
 
-// 			return connect.NewResponse(&apiv2.TokenServiceCreateResponse{Token: &apiv2.Token{Uuid: "abc"}}), nil
-// 		},
-// 		connect.WithInterceptors(interceptor),
-// 	))
-// 	srv := startHTTPServer(t, mux)
+	req := &apiv2.TokenServiceCreateRequest{
+		Description: `
+aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+`,
+	}
+	_, err := apiv2connect.NewTokenServiceClient(srv.Client(), srv.URL).Create(t.Context(), req)
+	require.Error(t, err)
+	require.EqualError(t, err, "invalid_argument: validation error:\n - description: must be shorter than 256 characters [string.is_description]")
+	require.Equal(t, connect.CodeInvalidArgument, connect.CodeOf(err))
+}
 
-// 	req := &apiv2.TokenServiceCreateRequest{
-// 		Description: `
-// aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
-// aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
-// aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
-// aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
-// aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
-// aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
-// aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
-// aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
-// `,
-// 	}
-// 	_, err := apiv2connect.NewTokenServiceClient(srv.Client(), srv.URL).Create(t.Context(), req)
-// 	require.Error(t, err)
-// 	require.EqualError(t, err, "invalid_argument: validation error:\n - description: must be shorter than 256 characters [string.is_description]")
-// 	require.Equal(t, connect.CodeInvalidArgument, connect.CodeOf(err))
-// }
-
-// func startHTTPServer(tb testing.TB, h http.Handler) *httptest.Server {
-// 	tb.Helper()
-// 	srv := httptest.NewUnstartedServer(h)
-// 	srv.EnableHTTP2 = true
-// 	srv.Start()
-// 	tb.Cleanup(srv.Close)
-// 	return srv
-// }
-
-// func createToken(ctx context.Context, _ *connect.Request[*apiv2.TokenServiceCreateRequest]) (*connect.Response[*apiv2.TokenServiceCreateResponse], error) {
-
-// 	return &connect.Response[&apiv2.TokenServiceCreateResponse{Token: &apiv2.Token{Uuid: "abc"}}], nil
-
-// }
+func startHTTPServer(tb testing.TB, h http.Handler) *httptest.Server {
+	tb.Helper()
+	srv := httptest.NewUnstartedServer(h)
+	srv.EnableHTTP2 = true
+	srv.Start()
+	tb.Cleanup(srv.Close)
+	return srv
+}
