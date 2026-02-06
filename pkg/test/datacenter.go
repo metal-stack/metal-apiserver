cc9345f9fbe20560ded9a3a635224137d5e1fc8d
Stefan Majer <stefan.majer@f-i-ts.de>
2026-02-06 14:47:51 +0100

    Initial

---
+++ pkg/test/datacenter.go
@@ -0,0 +1,236 @@
+package test
+
+import (
+	"fmt"
+	"maps"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	adminv2 "github.com/metal-stack/api/go/metalstack/admin/v2"
+	apiv2 "github.com/metal-stack/api/go/metalstack/api/v2"
+	"github.com/metal-stack/metal-apiserver/pkg/repository"
+	"github.com/metal-stack/metal-lib/pkg/pointer"
+)
+
+type (
+	DatacenterConfig struct {
+		Partitions        *uint
+		Racks             *uint
+		SwitchesPerRack   *uint
+		MachinesPerRack   *uint
+		Tenants           *uint
+		ProjectsPerTenant *uint
+	}
+
+	Datacenter struct {
+		Tenants    []string
+		Projects   map[string]string
+		Partitions map[string]*Partition
+		Sizes      map[string]*apiv2.Size
+		Networks   map[string]*apiv2.Network
+		IPs        map[string]*apiv2.IP
+		Images     map[string]*apiv2.Image
+
+		testStore *testStore
+		t         testing.TB
+		closers   []func()
+	}
+
+	Partition struct {
+		Racks map[string]*Rack
+	}
+
+	Rack struct {
+		Switches map[string]*apiv2.Switch
+		Machines map[string]*apiv2.Machine
+	}
+)
+
+func NewDatacenter(t testing.TB, testStore *testStore, config *DatacenterConfig) *Datacenter {
+	dc := &Datacenter{
+		testStore: testStore,
+		t:         t,
+	}
+
+	dc.createTenantsProjectsAndMembers()
+	dc.createPartitions(config.Partitions)
+	dc.createImagesAndSizes()
+	dc.createIPsAndNetworks()
+	dc.createMachines()
+	dc.createSwitches()
+
+	return dc
+}
+
+func (dc *Datacenter) Close() {
+	for _, close := range dc.closers {
+		close()
+	}
+}
+
+func (dc *Datacenter) createTenantsProjectsAndMembers() {
+	dc.Tenants = CreateTenants(dc.t, dc.testStore, []*apiv2.TenantServiceCreateRequest{
+		{Name: "john.doe@github.com"},
+		{Name: "foo.bar@github.com"},
+		{Name: "viewer@github.com"},
+		{Name: "ansible"},
+		{Name: "metal-image-cache-sync"},
+		{Name: "metal-hammer"},
+		{Name: "pixiecore"},
+	})
+	CreateTenantMemberships(dc.t, dc.testStore, "john.doe@github.com", []*repository.TenantMemberCreateRequest{
+		{MemberID: "john.doe@github.com", Role: apiv2.TenantRole_TENANT_ROLE_OWNER},
+		{MemberID: "viewer@github.com", Role: apiv2.TenantRole_TENANT_ROLE_VIEWER},
+	})
+	CreateTenantMemberships(dc.t, dc.testStore, "viewer@github.com", []*repository.TenantMemberCreateRequest{
+		{MemberID: "viewer@github.com", Role: apiv2.TenantRole_TENANT_ROLE_OWNER},
+	})
+	CreateTenantMemberships(dc.t, dc.testStore, "foo.bar@github.com", []*repository.TenantMemberCreateRequest{
+		{MemberID: "foo.bar@github.com", Role: apiv2.TenantRole_TENANT_ROLE_OWNER},
+	})
+	CreateTenantMemberships(dc.t, dc.testStore, "metal-image-cache-sync", []*repository.TenantMemberCreateRequest{
+		{MemberID: "metal-image-cache-sync", Role: apiv2.TenantRole_TENANT_ROLE_OWNER},
+	})
+	CreateTenantMemberships(dc.t, dc.testStore, "metal-hammer", []*repository.TenantMemberCreateRequest{
+		{MemberID: "metal-hammer", Role: apiv2.TenantRole_TENANT_ROLE_OWNER},
+	})
+	CreateTenantMemberships(dc.t, dc.testStore, "pixiecore", []*repository.TenantMemberCreateRequest{
+		{MemberID: "pixiecore", Role: apiv2.TenantRole_TENANT_ROLE_OWNER},
+	})
+	CreateTenantMemberships(dc.t, dc.testStore, "ansible", []*repository.TenantMemberCreateRequest{
+		{MemberID: "ansible", Role: apiv2.TenantRole_TENANT_ROLE_OWNER},
+	})
+	dc.Projects = CreateProjects(dc.t, dc.testStore, []*apiv2.ProjectServiceCreateRequest{
+		{Login: "john.doe@github.com"},
+	})
+	CreateProjectMemberships(dc.t, dc.testStore, dc.Projects["john.doe@github.com"], []*repository.ProjectMemberCreateRequest{
+		{TenantId: "foo.bar@github.com", Role: apiv2.ProjectRole_PROJECT_ROLE_VIEWER},
+	})
+}
+
+func (dc *Datacenter) createPartitions(count *uint) {
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		_, _ = fmt.Fprintln(w, "a image")
+	}))
+	dc.closers = append(dc.closers, ts.Close)
+
+	validURL := ts.URL
+
+	if count == nil {
+		count = pointer.Pointer(uint(1))
+	}
+
+	var partitions []*adminv2.PartitionServiceCreateRequest
+	for i := range *count {
+		id := fmt.Sprintf("partition-%d", i)
+		partitions = append(partitions, &adminv2.PartitionServiceCreateRequest{
+			Partition: &apiv2.Partition{
+				Id: id,
+				BootConfiguration: &apiv2.PartitionBootConfiguration{
+					ImageUrl:  validURL,
+					KernelUrl: validURL,
+				},
+			},
+		})
+		dc.Partitions[id] = &Partition{}
+	}
+
+	CreatePartitions(dc.t, dc.testStore, partitions)
+}
+
+func (dc *Datacenter) createImagesAndSizes() {
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		_, _ = fmt.Fprintln(w, "a image")
+	}))
+	dc.closers = append(dc.closers, ts.Close)
+
+	dc.Images = CreateImages(dc.t, dc.testStore, []*adminv2.ImageServiceCreateRequest{
+		{
+			Image: &apiv2.Image{Id: "debian-12.0.20241231", Url: ts.URL, Features: []apiv2.ImageFeature{apiv2.ImageFeature_IMAGE_FEATURE_MACHINE}},
+		},
+		{
+			Image: &apiv2.Image{Id: "firewall-ubuntu-3.0.20241231", Url: ts.URL, Features: []apiv2.ImageFeature{apiv2.ImageFeature_IMAGE_FEATURE_FIREWALL}},
+		},
+		{
+			Image: &apiv2.Image{Id: "debian-13.0.20241231", Url: ts.URL, Features: []apiv2.ImageFeature{apiv2.ImageFeature_IMAGE_FEATURE_MACHINE}},
+		},
+	})
+
+	dc.Sizes = CreateSizes(dc.t, dc.testStore, []*adminv2.SizeServiceCreateRequest{
+		{
+			Size: &apiv2.Size{
+				Id: "n1-medium-x86", Name: pointer.Pointer("n1-medium-x86"),
+				Constraints: []*apiv2.SizeConstraint{
+					{Type: apiv2.SizeConstraintType_SIZE_CONSTRAINT_TYPE_CORES, Min: 4, Max: 4},
+					{Type: apiv2.SizeConstraintType_SIZE_CONSTRAINT_TYPE_MEMORY, Min: 1024 * 1024, Max: 1024 * 1024},
+					{Type: apiv2.SizeConstraintType_SIZE_CONSTRAINT_TYPE_STORAGE, Min: 10 * 1024 * 1024, Max: 10 * 1024 * 1024},
+				},
+			},
+		},
+		{
+			Size: &apiv2.Size{
+				Id: "c1-large-x86", Name: pointer.Pointer("c1-large-x86"),
+				Constraints: []*apiv2.SizeConstraint{
+					{Type: apiv2.SizeConstraintType_SIZE_CONSTRAINT_TYPE_CORES, Min: 8, Max: 8},
+					{Type: apiv2.SizeConstraintType_SIZE_CONSTRAINT_TYPE_MEMORY, Min: 1024 * 1024, Max: 1024 * 1024},
+					{Type: apiv2.SizeConstraintType_SIZE_CONSTRAINT_TYPE_STORAGE, Min: 10 * 1024 * 1024, Max: 10 * 1024 * 1024},
+				},
+			},
+		},
+	})
+}
+
+func (dc *Datacenter) createIPsAndNetworks() {
+
+	for partition := range dc.Partitions {
+
+		networks := CreateNetworks(dc.t, dc.testStore, []*adminv2.NetworkServiceCreateRequest{
+			{
+				Id:                       pointer.Pointer("tenant-super-network-" + partition),
+				Prefixes:                 []string{"10.100.0.0/14"},
+				DefaultChildPrefixLength: &apiv2.ChildPrefixLength{Ipv4: pointer.Pointer(uint32(22))},
+				Type:                     apiv2.NetworkType_NETWORK_TYPE_SUPER,
+				Partition:                &partition,
+			},
+			{
+				Id:        pointer.Pointer("underlay-" + partition),
+				Name:      pointer.Pointer("Underlay Network"),
+				Partition: &partition,
+				Prefixes:  []string{"10.0.0.0/24"},
+				Type:      apiv2.NetworkType_NETWORK_TYPE_UNDERLAY,
+			},
+		})
+		maps.Copy(dc.Networks, networks)
+	}
+
+	internet := CreateNetworks(dc.t, dc.testStore, []*adminv2.NetworkServiceCreateRequest{
+		{
+
+			Id:                  pointer.Pointer("internet"),
+			Prefixes:            []string{"1.2.3.0/24"},
+			DestinationPrefixes: []string{"0.0.0.0/0"},
+			Vrf:                 pointer.Pointer(uint32(11)),
+			Type:                apiv2.NetworkType_NETWORK_TYPE_EXTERNAL,
+		},
+	})
+	maps.Copy(dc.Networks, internet)
+
+	for partition := range dc.Partitions {
+		for project := range dc.Projects {
+			allocatedNetworks := AllocateNetworks(dc.t, dc.testStore, []*apiv2.NetworkServiceCreateRequest{
+				{Name: pointer.Pointer(project + "-network-a-" + partition), Project: project, Partition: &partition},
+				{Name: pointer.Pointer(project + "-network-b-" + partition), Project: project, Partition: &partition},
+			})
+			maps.Copy(dc.Networks, allocatedNetworks)
+		}
+	}
+}
+
+func (dc *Datacenter) createMachines() {
+
+}
+
+func (dc *Datacenter) createSwitches() {
+
+}
--- pkg/test/repository.go
+++ pkg/test/repository.go
@@ -220,7 +220,7 @@ func (t *testStore) GetEventContainer(machineID string) *metal.ProvisioningEvent
 	return resp
 }
 
-func CreateImages(t *testing.T, testStore *testStore, images []*adminv2.ImageServiceCreateRequest) map[string]*apiv2.Image {
+func CreateImages(t testing.TB, testStore *testStore, images []*adminv2.ImageServiceCreateRequest) map[string]*apiv2.Image {
 	imageMap := map[string]*apiv2.Image{}
 	for _, img := range images {
 		i, err := testStore.Image().Create(t.Context(), img)
@@ -230,7 +230,7 @@ func CreateImages(t *testing.T, testStore *testStore, images []*adminv2.ImageSer
 	return imageMap
 }
 
-func CreateFilesystemLayouts(t *testing.T, testStore *testStore, fsls []*adminv2.FilesystemServiceCreateRequest) map[string]*apiv2.FilesystemLayout {
+func CreateFilesystemLayouts(t testing.TB, testStore *testStore, fsls []*adminv2.FilesystemServiceCreateRequest) map[string]*apiv2.FilesystemLayout {
 	fslMap := map[string]*apiv2.FilesystemLayout{}
 	for _, fsl := range fsls {
 		fsl, err := testStore.FilesystemLayout().Create(t.Context(), fsl)
@@ -240,7 +240,7 @@ func CreateFilesystemLayouts(t *testing.T, testStore *testStore, fsls []*adminv2
 	return fslMap
 }
 
-func CreateIPs(t *testing.T, testStore *testStore, ips []*apiv2.IPServiceCreateRequest) map[string]*apiv2.IP {
+func CreateIPs(t testing.TB, testStore *testStore, ips []*apiv2.IPServiceCreateRequest) map[string]*apiv2.IP {
 	ipMap := map[string]*apiv2.IP{}
 	for _, ip := range ips {
 		i, err := testStore.UnscopedIP().Create(t.Context(), ip)
@@ -250,7 +250,7 @@ func CreateIPs(t *testing.T, testStore *testStore, ips []*apiv2.IPServiceCreateR
 	return ipMap
 }
 
-func CreateMachinesWithAllocation(t *testing.T, testStore *testStore, machines []*apiv2.MachineServiceCreateRequest) map[string]*apiv2.Machine {
+func CreateMachinesWithAllocation(t testing.TB, testStore *testStore, machines []*apiv2.MachineServiceCreateRequest) map[string]*apiv2.Machine {
 	machineMap := map[string]*apiv2.Machine{}
 	for _, machine := range machines {
 		m, err := testStore.UnscopedMachine().Create(t.Context(), machine)
@@ -382,7 +382,7 @@ func (t *testStore) DeleteProjectInvites() {
 	}
 }
 
-func AllocateNetworks(t *testing.T, testStore *testStore, nws []*apiv2.NetworkServiceCreateRequest) map[string]*apiv2.Network {
+func AllocateNetworks(t testing.TB, testStore *testStore, nws []*apiv2.NetworkServiceCreateRequest) map[string]*apiv2.Network {
 	networkMap := map[string]*apiv2.Network{}
 
 	for _, nw := range nws {
@@ -407,7 +407,7 @@ func AllocateNetworks(t *testing.T, testStore *testStore, nws []*apiv2.NetworkSe
 	return networkMap
 }
 
-func CreatePartitions(t *testing.T, testStore *testStore, partitions []*adminv2.PartitionServiceCreateRequest) map[string]*apiv2.Partition {
+func CreatePartitions(t testing.TB, testStore *testStore, partitions []*adminv2.PartitionServiceCreateRequest) map[string]*apiv2.Partition {
 	partitionMap := map[string]*apiv2.Partition{}
 	for _, partition := range partitions {
 		p, err := testStore.Partition().Create(t.Context(), partition)
@@ -441,7 +441,8 @@ func CreateProjectInvites(t testing.TB, testStore *testStore, invites []*apiv2.P
 	}
 }
 
-func CreateTenants(t testing.TB, testStore *testStore, tenants []*apiv2.TenantServiceCreateRequest) {
+func CreateTenants(t testing.TB, testStore *testStore, tenants []*apiv2.TenantServiceCreateRequest) []string {
+	var tenantList []string
 	for _, tenant := range tenants {
 		tok, err := testStore.tokenService.CreateApiTokenWithoutPermissionCheck(t.Context(), tenant.GetName(), &apiv2.TokenServiceCreateRequest{
 			Expires:   durationpb.New(time.Minute),
@@ -453,7 +454,9 @@ func CreateTenants(t testing.TB, testStore *testStore, tenants []*apiv2.TenantSe
 
 		_, err = testStore.Tenant().AdditionalMethods().CreateWithID(reqCtx, tenant, tenant.Name)
 		require.NoError(t, err)
+		tenantList = append(tenantList, tenant.Name)
 	}
+	return tenantList
 }
 
 func CreateTenantMemberships(t testing.TB, testStore *testStore, tenant string, memberships []*repository.TenantMemberCreateRequest) {
@@ -470,7 +473,7 @@ func CreateTenantInvites(t testing.TB, testStore *testStore, invites []*apiv2.Te
 	}
 }
 
-func CreateSizes(t *testing.T, testStore *testStore, sizes []*adminv2.SizeServiceCreateRequest) map[string]*apiv2.Size {
+func CreateSizes(t testing.TB, testStore *testStore, sizes []*adminv2.SizeServiceCreateRequest) map[string]*apiv2.Size {
 	sizeMap := map[string]*apiv2.Size{}
 	for _, size := range sizes {
 		s, err := testStore.Size().Create(t.Context(), size)
@@ -480,7 +483,7 @@ func CreateSizes(t *testing.T, testStore *testStore, sizes []*adminv2.SizeServic
 	return sizeMap
 }
 
-func CreateSwitches(t *testing.T, testStore *testStore, switches []*repository.SwitchServiceCreateRequest) map[string]*apiv2.Switch {
+func CreateSwitches(t testing.TB, testStore *testStore, switches []*repository.SwitchServiceCreateRequest) map[string]*apiv2.Switch {
 	switchMap := map[string]*apiv2.Switch{}
 	for _, sw := range switches {
 		s, err := testStore.Switch().Create(t.Context(), sw)
@@ -490,7 +493,7 @@ func CreateSwitches(t *testing.T, testStore *testStore, switches []*repository.S
 	return switchMap
 }
 
-func CreateSwitchStatuses(t *testing.T, testStore *testStore, statuses []*repository.SwitchStatus) map[string]*metal.SwitchStatus {
+func CreateSwitchStatuses(t testing.TB, testStore *testStore, statuses []*repository.SwitchStatus) map[string]*metal.SwitchStatus {
 	statusMap := map[string]*metal.SwitchStatus{}
 	for _, status := range statuses {
 		metalStatus := &metal.SwitchStatus{
