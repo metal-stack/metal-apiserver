dbf999c460041296aa3cf7285816af212c5eb311
Stefan Majer <stefan.majer@f-i-ts.de>
2026-06-24 15:04:15 +0200

    Fix and add tests

--- pkg/request/tokenpermissions_test.go
+++ pkg/request/tokenpermissions_test.go
@@ -104,6 +104,7 @@ func Test_getTokenPermissions(t *testing.T) {
 				"/metalstack.admin.v2.TaskService/Queues":                        {"*": {}},
 				"/metalstack.admin.v2.TenantService/Create":                      {"*": {}},
 				"/metalstack.admin.v2.TenantService/List":                        {"*": {}},
+				"/metalstack.admin.v2.TenantService/AddMember":                   {"*": {}},
 				"/metalstack.admin.v2.TokenService/Create":                       {"*": {}},
 				"/metalstack.admin.v2.TokenService/List":                         {"*": {}},
 				"/metalstack.admin.v2.TokenService/Revoke":                       {"*": {}},
